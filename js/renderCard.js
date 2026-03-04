11890f83188c32b81db7ba57272f1b5abce60786
8headswillroll8 <lejohansen91@gmail.com>
2026-03-04 23:03:18 +0100

    add hover interactions for cards, buttons, and links

--- js/renderCard.js
+++ js/renderCard.js
@@ -6,20 +6,20 @@ if (grid) {
   grid.innerHTML = projects
     .map(
       ({ title, description, image, alt, links }) => `
-<article class="card">
-<div class="card__media">
-<img class="card__img" src="${image}" alt="${alt}" >
-</div>
+    <article class="card">
+      <div class="card__media">
+      <img class="card__img" src="${image}" alt="${alt}" >
+      </div>
 
-    <div class="card__content">
-      <h2>${title}</h2>
-      <p>${description}</p>
+      <div class="card__content">
+        <h2>${title}</h2>
+        <p>${description}</p>
 
-      <div class="card__actions">
-        <a href="${links.live}" target="_blank" rel="noopener noreferrer">
-          <img src="../assets/icons/arrow-top-right.svg" alt="">
-          Open live
-        </a>
+        <div class="card__actions">
+          <a href="${links.live}" target="_blank" rel="noopener noreferrer">
+            <img src="../assets/icons/arrow-top-right.svg" alt="">
+            Open live
+          </a>
 
         <a href="${links.github}" target="_blank" rel="noopener noreferrer">
           <img src="../assets/icons/arrow-top-right.svg" alt="">
